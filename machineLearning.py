0676597bef51d5def653fa05aa63c2693bf74352
atikin <n.kister@tu-bs.de>
2017-04-07 19:21:24 +0200

    moved perceptron in its own file and moved the "test" in this file too

--- machineLearning.py
+++ machineLearning.py
@@ -1,54 +1,5 @@
 import numpy as np
-
-
-# just linear seperable problem
-class Perceptron2D:
-
-    learning_rate: float
-    weights: np.array
-
-    def __init__(self, learning_rate):
-        self.learning_rate = learning_rate
-
-    def fit(self, data, labels):
-        if data.shape[0] != labels.shape[0]:
-            raise Exception(
-                "Num of label must equal number of data: {:d} != {:d}".format(data.shape[0], labels.shape[0]))
-
-        self.weights = np.zeros(data.shape[1] + 1)
-        transformed_data = self.__transform(data, labels)  # transform for batch perceptron algorithm
-
-        false_classified_data = np.array([x for x in transformed_data if self.__discriminate(x) <= 0])
-
-        while false_classified_data.shape[0] > 0:
-            false_classified_data = np.array([x for x in transformed_data if self.__discriminate(x) <= 0])
-            print("False classifications {:d}".format(false_classified_data.shape[0]))
-            self.weights = self.weights + self.learning_rate * np.sum(false_classified_data, axis=0)
-
-    def __transform(self, data, labels):
-        transformed = np.array([np.append([1], x) for x in data])
-        for i, x in enumerate(transformed):
-            if labels[i] == 2:
-                transformed[i] = -x
-            else:
-                transformed[i] = x
-        return transformed
-
-    def predict(self, data):
-        labels = np.empty((data.shape[0]))
-        transformed_data = np.array([np.append([1], x) for x in data])  # transform for batch perceptron algorithm
-        for i, instance in enumerate(transformed_data):
-            classification = self.__discriminate(instance)
-            if classification > 0:
-                labels[i] = 1
-            elif classification < 0:
-                labels[i] = 2
-            else:  # set to 0 if cant decide
-                labels[i] = 0
-        return labels
-
-    def __discriminate(self, x):
-        return np.dot(self.weights, x)
+import Perceptron
 
 
 # TODO: change the fit interface. fit should take a data vector and a label vector
